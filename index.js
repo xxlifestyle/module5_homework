1f286395ac468f540145cf1d9ebcecc67ad203c3
Vladislav <vladcherepanov0@gmail.com>
2021-03-25 19:52:25 +0300

    solution

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-{\rtf1}
+let map = new Map();
+map.set(false, "boolean");
+map.set("Moscow", "City");
+map.set(123, "number");
+map.set(true, "boolean");
+map.set(654, "number");
+for (let [key, value] of map) {
+  console.log(`Ключ - ${key}, Значение - ${value}`);
+}
